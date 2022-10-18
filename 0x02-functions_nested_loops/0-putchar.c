740dd3be39fadd612a8775c909dc8faf42c0f1fd
oluku samson <uweomao@gmail.com>
2022-10-18 16:11:06 -0700

    saaam

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,62 @@
-#include "main.h"
+#include "main"
+
+#include <unisted.h>
+
 /**
- * main -print _putchar
- * created by sammy
- * Return: 0
- */
+
+*_putchar - writes the character c to stdout
+
+*@c: the character to print
+
+*Return: On success 1
+
+*/
+
+int _putchar(char c)
+
+{
+
+return (write (1, &c, 1));
+
+}
+
+
+0.  #include "main.h"
+
+/**
+
+*main - Entry point
+
+*Return: Always 0 (Success)
+
+*/
 
 int main(void)
+
 {
+
 _putchar('_');
+
 _putchar('p');
+
 _putchar('u');
+
 _putchar('t');
+
 _putchar('c');
+
 _putchar('h');
+
 _putchar('a');
+
 _putchar('r');
+
 _putchar('\n');
-return(0);
+
+
+return (0);
+
+}
+
 }
 
