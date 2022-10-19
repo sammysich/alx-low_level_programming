5a993790eb8c7270865687bb82f5770b45a8b16b
oluku samson <uweomao@gmail.com>
2022-10-19 12:19:11 -0700

    saamm

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,18 +2,16 @@
 /**
  * print_alphabet_x10 - Make alphabet x10 times
  * Return: void
- */
+*/
 void print_alphabet_x10(void)
 {
+	int count = 0;
 	char c;
-	int i = 0;
-	while (i <= 9)
-		{
+
+	while (c++ <= 9)
+	{
 		for (c = 'a'; c <= 'z'; c++)
-		{
 			 _putchar(c);
-		}
-			_putchar('\n');
-			i++
-		}
+		_putchar('\n');
+	}
 }
